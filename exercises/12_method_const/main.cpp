a6b3fb5054b9d43da2dc426e1f8fbccf1ad16a59
crapromer <tianniehao@hotmail.com>
2024-12-17 17:07:48 +0800

    finishi 12

--- exercises/12_method_const/main.cpp
+++ exercises/12_method_const/main.cpp
@@ -6,6 +6,11 @@ struct Fibonacci {
     int numbers[11];
     // TODO: 修改方法签名和实现，使测试通过
     int get(int i) {
+        return numbers[i];
+    }
+    Fibonacci(const initializer_list<int> nums){
+        for(int i = 0;i<nums.length();i++)
+            numbers[i] = nums[i];
     }
 };
 
